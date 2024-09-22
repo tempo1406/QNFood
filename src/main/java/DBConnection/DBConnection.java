1121ee773cbbe8981afee7491d045875ea2a1cdc
tempo1406 <134191285+tempo1406@users.noreply.github.com>
2024-09-22 23:03:55 +0700

    First Commit

---
+++ src/main/java/DBConnection/DBConnection.java
@@ -0,0 +1,36 @@
+package DBConnection;
+    
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnection {
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            try {
+                conn = DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=qnfood;user=sa;password=123;encrypt=true;trustServerCertificate=true");
+            } catch (SQLException ex) {
+                Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+    
+    public static void main(String[] args) {
+        DBConnection dbConnection = new DBConnection();
+        Connection connection = dbConnection.getConnection();
+        if (connection != null) {
+            System.out.println("Connect successfully");
+        } else {
+            System.out.println("Connect fail");
+        }
+    }
+}
